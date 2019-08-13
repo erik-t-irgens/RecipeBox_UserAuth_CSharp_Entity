dc5a6e41a4b0529dedcf8658659e802201dc38b2
Erik Irgens and Na Hyung Choi <erik@>
2019-08-13 12:01:47 -0700

    initial commit

---
+++ RecipeBox/Models/RecipeBoxContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace RecipeBox.Models
+{
+    public class RecipeBoxContext : IdentityDbContext<ApplicationUser>
+    {
+        public virtual DbSet<
+    }
+}
